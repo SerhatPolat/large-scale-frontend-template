451ecfb39cbaa1620e7d7b7278fa63882a415e82
Serhat Polat <SerhatLiving@hotmail.com>
2023-01-08 10:29:02 +0300

    Installed react-icons and Added an example usage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getExampleData } from "../redux/slices/ExampleSlice";
 import { AppDispatch } from "../redux/store";
 
+import { FaRegSmile } from "react-icons/fa";
+
 function Home() {
   const dispatch = useDispatch<AppDispatch>();
 
@@ -18,10 +20,10 @@ function Home() {
       <BaseComponent text="Component props works" darkmode />
 
       <button
-        className="bg-green-500 text-white rounded-r-full my-6 p-4"
+        className="flex items-center bg-green-500 text-white rounded-r-full my-6 p-4"
         onClick={fetchDataFunc}
       >
-        Click to fetch data
+        Click to fetch data <FaRegSmile size={30} className="ml-2" />
       </button>
 
       <p>{error ? "Errorrrr!" : exampleResp ? exampleResp.name : ""}</p>
